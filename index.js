9d69a0571c38ccd6879822c556cf33bb677acab0
QuagKai <119025914+QuagKai@users.noreply.github.com>
2023-05-09 00:47:25 +0700

    Upload files
    - Upload the body header including navigation bar
    - Upload the body content of homepage
    - Render home.ejs when using port 3000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello welcome')
+    res.render('home')
 });
 
 app.listen(port, () => {
