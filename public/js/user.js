642be594dde4965adba7ebdf68e9fa4911c5a691
QuagKai <119025914+QuagKai@users.noreply.github.com>
2023-05-08 23:49:33 +0700

    Upload files
    - Upload pre-existing files
    - Create .gitignore ignore the node_modules folder and .env file

---
+++ public/js/user.js
@@ -0,0 +1,39 @@
+const users = [
+{
+    id: 1,
+    name: 'John',
+    username: 'JohnCena',
+    password: 'youcantseeme',
+    email: 'johncena@user.com',
+    role: 'user'
+},
+
+{
+    id: 2,
+    name: 'Rump',
+    username: 'RumpBach',
+    password: 'rumrum123',
+    email: 'rumbach@user.com',
+    role: 'vendoe'
+},
+
+{
+    id: 3,
+    name: 'Tom',
+    username: 'TomSean',
+    password: 'tomtom123',
+    email: 'tomsean@user.com',
+    role: 'shipper'
+},
+
+{
+    id: 4,
+    name: 'Admin',
+    username: 'Admin',
+    password: 'admin123',
+    email: 'Admin@user.com',
+    role: 'admin'
+}
+]
+
+module.exports = users;
